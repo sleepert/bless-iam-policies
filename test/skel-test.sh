f5a2fb31f90b5a9279f81ce78aa9213cd0bd8fcb
sleepert <jterry026@gmail.com>
2018-12-19 10:00:20 +0000

    add skel test

---
+++ test/skel-test.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+USER="testuser"
+HOME_DIR="/home/$USER"
+useradd -md $HOME_DIR $USER
+
+echo "Testing .ssh directory"
+if [ ! -d "$HOME_DIR/.ssh" ]; then
+  echo "Missing .ssh"
+  exit 1
+fi
+
+echo "test authorized_keys exists..."
+if [ ! -f "$HOME_DIR/.ssh/authorized_keys" ]; then
+  echo "missing authorized_keys"
+  exit 1
+fi
+
+echo "test empty authorized_keys"
+if [ ! -s "$HOME_DIR/.ssh/authorized_keys" ]; then
+  echo "authorized_keys contains data it shouldnt"
+  exit 1
+fi
+
+echo "test bless_client exits"
+if [ ! -f "$HOME_DIR/bless_client.py" ]; then
+  echo "bless_client missing from home directory"
+  exit 1
+fi
+
+echo "test temporary key exists"
+if [ ! -f "$HOME_DIR/.ssh/blessid" ]; then
+  echo "private key missing"
+  exit 1
+fi
+
+echo "test temporary cert exists"
+if [ ! -f "$HOME_DIR/.ssh/blessid-cert" ]; then
+  echo "temp cert missing"
+  exit 1
+fi
+
+sudo -u $USER bash << EOF
+echo "as user $USER"
+if [ `whoami != $USER ]; then
+  echo "wrong user: $(whoami)"
+  exit 1
+fi
+echo "test bless alias is available"
+if [ `alias | grep bless | wc -l` != 1 ]; then
+  echo "bless alias does not exist"
+  exit 1
+fi
+EOF
